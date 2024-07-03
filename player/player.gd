951254c61d5839b3a299fafa07b663ceb6bfca6f
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-03 01:33:37 -0300

    add player walk aninmation

--- player/player.gd
+++ player/player.gd
@@ -3,32 +3,40 @@ extends CharacterBody2D
 var speed = 500
 var can_shoot: bool = true
 var ice_spear = preload("res://projectiles/ice_spear.tscn")
+@onready var walkTimer = get_node("walk_timer")
+@onready var player_image  = get_node("player_image")
 
 func get_input():
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	if Input.is_action_just_pressed("sprint"):
 		speed = 600
-	if direction.x > 0:
-		$Sprite2D.flip_h = true
-	elif direction.x <0:
-		$Sprite2D.flip_h = false
-	
-	if velocity.x > 0:
-		$Sprite2D.frame = 0
-	else:
-		toggle_frames()
 	
 	if Input.is_action_just_pressed("primary") and can_shoot:
 		can_shoot = false
 		shoot()
 		$shoot_timer.start()
+	
+	asset_changes(direction)
+
+func asset_changes(direction):
+	if direction.x > 0:
+		player_image.flip_h = true
+	elif direction.x <0:
+		player_image.flip_h = false
+	
+	if velocity == Vector2.ZERO:
+		player_image.frame = 0
+	if velocity != Vector2.ZERO:
+		if walkTimer.is_stopped():
+			toggle_frames()
+			walkTimer.start()
 
 func toggle_frames():
-	if $Sprite2D.frame == 0:
-		$Sprite2D.frame = 1
+	if player_image.frame == 0:
+		player_image.frame = 1
 	else:
-		$Sprite2D.frame = 0
+		player_image.frame = 0
 
 func _physics_process(delta):
 	get_input()
@@ -43,3 +51,7 @@ func shoot():
 func _on_shoot_timer_timeout():
 	can_shoot = true
 
+
+
+func _on_walk_timer_timeout():
+	pass # Replace with function body.
