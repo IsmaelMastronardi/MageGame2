5d57a4ceedd3892537c563cdc8b4679f4fc85b34
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-04 00:41:20 -0300

    add spawner and mob clasess

--- enemies/mobs/mob.gd
+++ enemies/mobs/mob.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name Mob
+
 @export var speed = 80
 @onready var player = get_tree().get_first_node_in_group("player")
 
@@ -7,3 +9,4 @@ func _physics_process(delta):
 	var direction = position.direction_to(player.position)
 	velocity = direction * speed
 	move_and_slide()
+
