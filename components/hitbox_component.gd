fb07fbb9d1a1515003e83c270a0efca035792ef7
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-08 00:32:25 -0300

    add hitbox, health and hurtbox component

---
+++ components/hitbox_component.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+class_name HitBoxComponent
+
+@export var hitbox : CollisionShape2D
+@export var damage : int = 100
+
+func deal_damage():
+	pass
+	
+
--- projectiles/ice_spear.gd
+++ projectiles/ice_spear.gd
@@ -6,4 +6,4 @@ var missile_direction
 
 func _process(delta):
 	position -= missile_direction * speed * delta
-	$IceSpear.rotation = missile_direction.angle() + deg_to_rad(135)
+	rotation = missile_direction.angle() + deg_to_rad(135)
